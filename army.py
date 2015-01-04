67ba5ce2d12207ff5a7fccd9f71cf55930bf44b5
Omega <omega@alpha.org>
2015-01-03 19:02:30 -0500

    first commit

--- army.py
+++ army.py
@@ -23,50 +23,39 @@ def receive(s,channel):
 			shit, hashbit = msg.split("PING ")
 			hashbit = hashbit.strip()
 			s.send("PONG " + hashbit + "\r\n")
-		if msg.find(".join ") != -1:
-			shit, channel = msg.split(".join ")
-			name = new_owner.strip()     
-			s.send("JOIN " + channel + "\r\n")       
-		if msg.find(".penis") != -1:
-			s.send('PRIVMSG ' + channel + ' :IM GAY :^)\r\n')   
-		if msg.find(".say ") != -1:
-			try:
-				p = msg.split(".say ")[1].strip()
-				s.send('PRIVMSG ' + channel + ' :' + p +'\r\n')   
-			except Exception as e:
-				pass
-		if msg.find ('.ip ') != -1:
-			try:
-				com, site = msg.split('.ip')
-				site = site.strip()
-				ip = socket.gethostbyname(site)
-				s.send('PRIVMSG ' + channel + ' :' + site + "'s ip is - " + ip + "\r\n")					
-			except(ValueError, socket.gaierror):	
-				s.send('PRIVMSG ' + channel + ' :Incorrect usage of command\r\n')
+		if "PRIVMSG " in msg:
+			channel = msg.split("PRIVMSG ")
+			channel = channel[1].split(" :")
+			channel = channel[0].strip()
+			print channel
+			sData = data.split(" PRIVMSG "+channel+" :")[1].strip()
+			if sData.startswith(".join "):
+				shit, channel = msg.split(".join ")   
+				s.send("JOIN " + channel + "\r\n")       
+			if sData.startswith(".penis"):
+				s.send('PRIVMSG ' + channel + ' :IM GAY :^)\r\n')   
+			if sData.startswith(".say "):
+				try:
+					p = msg.split(".say ")[1].strip()
+					s.send('PRIVMSG ' + channel + ' :' + p +'\r\n')   
+				except Exception as e:
+					pass
+			if sData.startswith('.ip '):
+				try:
+					com, site = msg.split('.ip')
+					site = site.strip()
+					ip = socket.gethostbyname(site)
+					s.send('PRIVMSG ' + channel + ' :' + site + "'s ip is - " + ip + "\r\n")					
+				except(ValueError, socket.gaierror):	
+					s.send('PRIVMSG ' + channel + ' :Incorrect usage of command\r\n')
 def Soldier():
-	nick = "R" + str(random.randint(0, 1000))
+	nick = "R" + str(random.randint(0, 10000))
 	server = "irc.tm"
 	port = 6667
 	channel = "#raid"
-	#tor = "127.0.0.1"
-	#tor_port = 9050
-	name = "Tetra"
-	vhost="!username@bugatti"
-	#s = socks.socksocket()
-	#s = socket.socket()
-	#sock = open("socks5.txt", "r+")
-	#list = sock.readlines()
-	#sock.close()
-	#proxies = []
-	#for line in list:
-#		pq = line.split(":")
-		#proxies.append((pq[0].strip(), int(pq[1].strip())))
-	#proxy = random.choice(proxies)
-	#print str(proxy)+"\n"
 	#Connect to irc server
 	s = socks.socksocket()
 	s.setproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-	#s.setproxy(socks.PROXY_TYPE_SOCKS5,proxy[0],proxy[1])
 	s.connect(("irc.tm",6667))
 	s.send("NICK " + nick + "\r\n")
 	s.send("USER " + nick + " " + nick + " " + nick + " "+nick+"\r\n")
@@ -78,7 +67,7 @@ def NewTorIP():
 		controller.signal(Signal.NEWNYM)
 oldIP = "0.0.0.0"
 newIP = "0.0.0.0"
-for n in range(30):
+for n in range(2):
 	thread = threading.Thread(target=Soldier)
 	thread.start()
 	threads.append(thread)
